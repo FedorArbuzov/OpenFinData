6c4b9494e56a3497a3499be253c909fa3dfcc720
Fedor Arbuzov <ArbuzovFP@yandex.ru>
2016-08-04 20:35:13 +0400

    Create work_class.py

---
+++ work_class.py
@@ -0,0 +1,10 @@
+class quest:
+    """A simple example class"""
+    subject = ""
+    place = ""
+    year = 0
+    sector = ""
+    planned_or_actual = ""
+
+    def f(self):
+        return 'hello world'
