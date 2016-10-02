ef55194b10af08dfdf0ce9f06f260308738950b2
DmitryEliseev <dima-eliseev10@rambler.ru>
2016-10-02 14:43:31 +0300

    changed telegram token to Datatron

--- m1_main.py
+++ m1_main.py
@@ -17,7 +17,7 @@ from m1_speechkit import SpeechException
 from m2_main import M2Retrieving
 from m3_main import M3Visualizing
 
-API_TOKEN = config.TELEGRAM_API_TOKEN1
+API_TOKEN = config.TELEGRAM_API_TOKEN_FINAL
 bot = telebot.TeleBot(API_TOKEN)
 
 
