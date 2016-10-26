9e1149d0761c885478f5db983ac40547b61480c3
imon <imon>
2016-10-26 15:17:47 +0300

    Some prints and rectifications

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ TELEGRAM_API_TOKEN2 = '250645074:AAF4vfI4wY177VWQYNzPBAt-JYFVyAWyn1I'
 TELEGRAM_API_TOKEN3 = '256228310:AAHUrblIlUY2Py-yXEIlajP4y7Wc3BArnHE'  # SvetaBot
 TELEGRAM_API_TOKEN_FINAL = '242536845:AAELC6ay-SVnaw0nIZHCZpsqw_KN8yiaD1U'  # Datatron
 
-FFMPEG_PATH_DIMA = r'C:\Users\User\PycharmProjects\OpenFinData\ffmpeg\bin\ffmpeg.exe'
+FFMPEG_PATH_DIMA = r'C:\Users\imon\Documents\Git\OpenFinData\ffmpeg\bin\ffmpeg.exe'
 FFMPEG_PATH_MASHA = r'C:\Users\The Cat Trex\PycharmProjects\OpenFinData\ffmpeg\bin\ffmpeg.exe'
